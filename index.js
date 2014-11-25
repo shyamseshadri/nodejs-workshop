0d3225255c69715941b5b63f0fc7b87d9fa67211
Shyam Seshadri <shyamseshadri@gmail.com>
2014-11-25 16:47:43 +0530

    Session 1 - Step 4: File reading using streams

--- index.js
+++ index.js
@@ -2,5 +2,17 @@
 var fs = require('fs'),
   filename = process.argv[2];
 
-var fileContent = fs.readFileSync(filename).toString();
-console.log("File contents are", fileContent);
+var readStream = fs.createReadStream(filename);
+
+readStream.on('data', function(data) {
+  console.log("Reading file data event : ", data.toString());
+});
+readStream.on('end', function() {
+  console.log('file ended');
+});
+
+readStream.on('error', function(e) {
+  console.log('error found', e);
+});
+
+
