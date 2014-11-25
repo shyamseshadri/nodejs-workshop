aad67e642eada1c575a1fb6fdab809c569deec76
Shyam Seshadri <shyamseshadri@gmail.com>
2014-11-25 16:32:18 +0530

    Session 1 - Step 2: Handling arguments and input from command line

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log("Hello World ...!", "Shyam", {key: {msg: 'Hello'}});
+
+
+console.log('Process Argv is ',  process.argv);
+console.log('First argument is ', process.argv[2]);
+console.log('Number of arguments are ', process.argv.length - 2);
