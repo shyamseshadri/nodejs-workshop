2e8fd3ad10deef2ac26e3f6490bbc3061ccc198a
Shyam Seshadri <shyamseshadri@gmail.com>
2014-12-01 18:13:42 +0530

    Session 2 - Step 4: Fetch basic teams data

--- index.js
+++ index.js
@@ -8,7 +8,27 @@ var app = express();
 
 
 app.get('/', function(req, res) {
-  res.send('Hello world');
+  async.waterfall([function(callback) {
+    teamCtrl.getTeams(callback);
+  }, function(teams, callback) {
+    var functionArray = [];
+    for (var i = 0; i < teams.length; i++) {
+      functionArray.push(function(team) {
+          return function(cb) {
+            teamCtrl.addMoreData(team, cb)
+          };
+        }(teams[i])
+      );
+    }
+    async.parallel(functionArray, function(err, teamsExtraInfo) {
+      for (var i = 0; i < teams.length; i++) {
+        teams[i].more = teamsExtraInfo[i];
+      }
+      callback(null, teams);
+    });
+  }], function(err, responses) {
+    res.send(responses);
+  });
 });
 
 var server = app.listen(8000, function() {
