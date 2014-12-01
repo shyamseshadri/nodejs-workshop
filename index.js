98f4a5547cc8635666a1343a0562c5133f6df7e3
Shyam Seshadri <shyamseshadri@gmail.com>
2014-12-01 18:30:24 +0530

    Session 2 - Step 6: Hook up login route with hardcoded checks

--- index.js
+++ index.js
@@ -3,11 +3,28 @@ var async = require('async');
 var teamCtrl= require('./team.controller');
 
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();
 
+app.use(bodyParser.json());
 
-app.get('/', function(req, res) {
+app.use(express.static(__dirname + '/public'));
+
+app.post('/api/login', function(req, res) {
+
+  var username = req.body.username;
+  var password = req.body.password;
+  if (username === 'admin' && password === 'password') {
+    res.send({msg: 'Successful Login'});
+  } else {
+    res.status(4005).send({msg: 'FAILED Login'});
+  }
+
+
+});
+
+app.get('/api/teams', function(req, res) {
   async.waterfall([function(callback) {
     teamCtrl.getTeams(callback);
   }, function(teams, callback) {
