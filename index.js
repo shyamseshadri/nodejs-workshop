3758689fd4e3266e42cada2d134c3c4d730707dc
Shyam Seshadri <shyamseshadri@gmail.com>
2014-12-01 14:49:18 +0530

    Session 2 - Step 2: Use async library for waterfall and parallel

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 
+var async = require('async');
 var teamCtrl= require('./team.controller');
 
-teamCtrl.getTeams(function(err, data) {
-  var teams = JSON.parse(data);
-  var count = 0;
+async.waterfall([function(callback) {
+  teamCtrl.getTeams(callback);
+}, function(teams, callback) {
+  var functionArray = [];
   for (var i = 0; i < teams.length; i++) {
-    teamCtrl.addMoreData(teams[i], function() {
-      count++;
-      if (count == teams.length) {
-        console.log('Fully loaded teams ', teams);
-      }
-    });
+    functionArray.push(function(team) {
+        return function(cb) {
+          teamCtrl.addMoreData(team, cb)
+        };
+      }(teams[i])
+    );
   }
+  async.parallel(functionArray, function(err, teamsExtraInfo) {
+    for (var i = 0; i < teams.length; i++) {
+      teams[i].more = teamsExtraInfo[i];
+    }
+    callback(null, teams);
+  });
+}], function(err, responses) {
+  console.log('Teams info is ', responses);
 });
 
 
--- team.controller.js
+++ team.controller.js
@@ -3,12 +3,13 @@ var fs = require('fs');
 
 module.exports = {
   getTeams: function(cb) {
-    fs.readFile('teams.json', cb);
+    fs.readFile('teams.json', function(err, data) {
+      cb(null, JSON.parse(data));
+    });
   },
   addMoreData: function(team, cb) {
     fs.readFile(team.id + '.json', function(err, data) {
-      team.more = JSON.parse(data);
-      cb();
+      cb(null, JSON.parse(data));
     });
   }
 };
