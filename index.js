cee2ca4de4f869df28583f558bae39bcd5d97c04
Shyam Seshadri <shyamseshadri@gmail.com>
2014-12-01 18:09:50 +0530

    Session 2 - Step 3: Express basic app

--- index.js
+++ index.js
@@ -2,28 +2,18 @@
 var async = require('async');
 var teamCtrl= require('./team.controller');
 
-async.waterfall([function(callback) {
-  teamCtrl.getTeams(callback);
-}, function(teams, callback) {
-  var functionArray = [];
-  for (var i = 0; i < teams.length; i++) {
-    functionArray.push(function(team) {
-        return function(cb) {
-          teamCtrl.addMoreData(team, cb)
-        };
-      }(teams[i])
-    );
-  }
-  async.parallel(functionArray, function(err, teamsExtraInfo) {
-    for (var i = 0; i < teams.length; i++) {
-      teams[i].more = teamsExtraInfo[i];
-    }
-    callback(null, teams);
-  });
-}], function(err, responses) {
-  console.log('Teams info is ', responses);
+var express = require('express');
+
+var app = express();
+
+
+app.get('/', function(req, res) {
+  res.send('Hello world');
 });
 
+var server = app.listen(8000, function() {
+  console.log('App listening at http://localhost:%s', server.address().port);
+});
 
 
 
